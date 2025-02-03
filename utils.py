637cd372c0790a67b97d5fee81f90a86dc3ddf84
Shivam Singh <155867651+techvjyt@users.noreply.github.com>
2025-02-03 22:05:55 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION
+from info import AUTH_CHANNEL, REQUEST_TO_JOIN_MODE, LONG_IMDB_DESCRIPTION
 from imdb import Cinemagoer
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton, ChatJoinRequest
@@ -34,15 +34,31 @@ class temp(object):
 async def is_subscribed(bot, query, channel):
     btn = []
     for id in channel:
-        chat = await bot.get_chat(int(id))
         try:
-            await bot.get_chat_member(id, query.from_user.id)
-        except UserNotParticipant:
-            btn.append(
-                [InlineKeyboardButton(f'Join {chat.title}', url=chat.invite_link)]
-            )
-        except Exception as e:
-            pass
+            chat = await bot.get_chat(int(id))
+            title = chat.title
+        except:
+            title = "Group"
+        if REQUEST_TO_JOIN_MODE == True and join_db().isActive():
+            user = await join_db().get_user(query.from_user.id, int(id))
+            if user:
+                continue
+            else:
+                try:
+                    await bot.get_chat_member(int(id), query.from_user.id)
+                except UserNotParticipant:
+                    link = await bot.create_chat_invite_link(chat_id=int(id), creates_join_request=True)
+                    btn.append([InlineKeyboardButton(f'Join {title}', url=link.invite_link)])
+                except Exception as e:
+                    pass
+        else:
+            try:
+                await bot.get_chat_member(int(id), query.from_user.id)
+            except UserNotParticipant:
+                link = await bot.create_chat_invite_link(chat_id=int(id))
+                btn.append([InlineKeyboardButton(f'Join {title}', url=link.invite_link)])
+            except Exception as e:
+                pass
     return btn
 
 async def get_poster(query, bulk=False, id=False, file=None):
